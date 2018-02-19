b2e1f24b9df65d4fc5767a45e3acde4349fd6f5d
aolsson <axel.olsson@chas.se>
2018-02-19 15:50:57 +0100

    Initial commit

---
+++ src/component/Content/Content.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Content.css';
+
+class Content extends Component {
+  render() {
+    return (
+        <main className="App-content">
+            {this.props.children}
+        </main>
+    );
+  }
+}
+
+export default Content;
